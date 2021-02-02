3cdc9b0fd7aed85afcf010a657dc467884f7c12a
Chris Finch <tophfinch@gmail.com>
2021-02-01 19:57:20 -0700

    More Styling

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,5 +1,11 @@
 import home1 from "../img/home1.png";
 // Styled
+import {
+  StyledAbout,
+  StyledImage,
+  StyledDescription,
+  StyledHide,
+} from "../styles";
 import styled from "styled-components";
 
 const AboutSection = () => {
@@ -33,35 +39,5 @@ const AboutSection = () => {
 };
 
 // Styled Components
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const StyledDescription = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const StyledImage = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const StyledHide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
